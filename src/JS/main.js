203f402a83b30409e355b88dcee18200d8e33e4d
Danrlei Souza <132399000+danrleisnob@users.noreply.github.com>
2024-03-20 09:34:49 -0300

    Update main.js

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,6 +1,6 @@
 var usuarios = [
   { nome: "Usuário 1", email: "danrleimentalista@gmail.com", senha: "Gestor@01" },
-  { nome: "Usuário 3", email: " atend201@speedmais.com.br", senha: "vida2023" },
+  { nome: "Usuário 3", email: "atend201@speedmais.com.br", senha: "vida2023" },
 ];
 
 function login() {
