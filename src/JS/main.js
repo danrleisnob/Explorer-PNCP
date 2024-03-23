3dcae076d84c542330d148addee0c7c4a6f6f64a
Danrlei Souza <132399000+danrleisnob@users.noreply.github.com>
2024-03-23 16:24:55 -0300

    Update main.js

--- src/JS/main.js
+++ src/JS/main.js
@@ -2,7 +2,7 @@ var usuarios = [
   { nome: "Danrlei", email: "danrleimentalista@gmail.com", senha: "Gestor@01" },
   { nome: "Elizabeth", email: "elizabeth.moraes@speedmais.com.br", senha: "speed123" },
   { nome: "Jaciane", email: "jaciany.freitas@speedmais.com.br", senha: "vida2023" },
-  { nome: "Usuário 3", email: "danielle.barros@speedmais.com.br", senha: "speed123" },
+  { nome: "Usuário 3", email: "danielle.barros@speedmais.com.br", senha: "speed123" }
 ];
 
 function login() {
